61870d983491759cadafb54be650735a6b6cb8da
Andrei.Tyrin <andrei.tyrin@jetbrains.com>
2022-01-14 18:25:23 +0300

    reproducer for some strange slow analyzes

--- p2/build.gradle.kts
+++ p2/build.gradle.kts
@@ -5,49 +5,23 @@ plugins {
 kotlin {
     val macosA = macosX64("macosA")
     val macosB = macosX64("macosB")
+//    val macosB = linuxX64("macosB")  // for some reason way faster
 
-//    val linux = linuxX64("linux")
 
     val commonMain by sourceSets.getting
-    val commonTest by sourceSets.getting
-
     val nativeMain by sourceSets.creating{
         dependsOn(commonMain)
     }
-    val nativeTest by sourceSets.creating{
-        dependsOn(commonTest)
-    }
-
     val macosAMain by sourceSets.getting
     val macosBMain by sourceSets.getting
 
-    val macosATest by sourceSets.getting
-    val macosBTest by sourceSets.getting
-
-//    val linuxMain by sourceSets.getting
-//    val linuxTest by sourceSets.getting
-
 
     macosAMain.dependsOn(nativeMain)
-    macosBMain.dependsOn(nativeMain)
+//    macosBMain.dependsOn(nativeMain)   // uncomment me
 
-    macosATest.dependsOn(nativeTest)
-    macosBTest.dependsOn(nativeTest)
-
-
-//    linuxMain.dependsOn(nativeMain)
-//    linuxTest.dependsOn(nativeTest)
 
     macosA.compilations.getByName("main").cinterops.create("curl")
-    macosB.compilations.getByName("main").cinterops.create("curl")
-
     macosA.compilations.getByName("main").cinterops.create("sqlite")
-    macosB.compilations.getByName("main").cinterops.create("sqlite")
-
-//    linux.compilations.getByName("main").cinterops.create("curl")
-//    linux.compilations.getByName("main").cinterops.create("sqlite")
-
-
 
 
     sourceSets.all {
